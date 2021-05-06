6be22ed8486a8881259e4231425594593e017fda
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-06 20:50:07 +0900

    1回目提出

--- Task8/SecondViewController.swift
+++ Task8/SecondViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  Task8
 //
-//  Created by 三浦　一真 on 2021/04/28.
+//  Created by 三浦　一真 on 2021/05/06.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class SecondViewController: UIViewController {
+
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var slider: UISlider!
     
@@ -18,16 +18,14 @@ class ViewController: UIViewController {
         slider.value = delegate.sliderValue ?? 0.0
         decideLabelText()
     }
-    
+
     @IBAction private func decideValue(_ sender: Any) {
-        if let delegate = UIApplication.shared.delegate as? AppDelegate {
-            delegate.sliderValue = slider.value
-            decideLabelText()
-        }
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        delegate.sliderValue = slider.value
+        decideLabelText()
     }
     
     private func decideLabelText() {
         label.text = String(slider.value)
     }
 }
-
